844e9155f533b88269b191814679f50c775de1d8
Rodrogas666 <rodri123pineda@gmail.com>
2025-05-01 19:39:05 -0600

    reorganización de carpetas

--- views/public.php
+++
@@ -1,7 +0,0 @@
-<div class="welcome-container">
-    <div class="welcome-card">
-        <h1>Bienvenido al Sistema de Selecciones</h1>
-        <p>Colegio Don Bosco</p>
-        <a href="?view=login" class="btn btn-primary btn-block" id="btn-gotoLogin">Iniciar sesión</a>
-    </div>
-</div>
